7ab93787d1770fb06fb1defc044941577e540b48
Jen E <jenevans3ten@gmail.com>
2020-03-10 10:34:39 -0400

    header component

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,1 +1,26 @@
-document.querySelector('#app').innerText = "Hello, I'm here"
+// document.querySelector('#app').innerText = "Hello, I'm here"
+import Header from './components/Header';
+
+pageBuild()
+
+function pageBuild() {
+    header();
+}
+
+function header() {
+    const header = document.querySelector("#header");
+    header.innerHTML = Header();
+    // header.innerHTML = 
+    // `<nav class='nav__header'>
+    //     <ul>
+    //         <li class='nav__home'>Home</li>
+    //         <li class='nav__values'>Values</li>
+    //         <li class='nav__todos'>ToDos</li>
+    //     </ul>
+    // </nav> ` 
+  }
+  
+  function footer() {
+    const footer = document.querySelector("#footer");
+    footer.innerHTML = Footer();
+  }
