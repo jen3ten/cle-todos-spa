44185eef4bb543052a0cfe94901048b48a1c9002
Jen E <jenevans3ten@gmail.com>
2020-03-17 20:36:19 -0400

    updated startup with dependency injection and other services

--- backend/todos/Startup.cs
+++ backend/todos/Startup.cs
@@ -10,26 +10,41 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using todos.Models;
+using todos.Repositories;
 
 namespace todos
 {
     public class Startup
     {
+        // used for Web APIs, when IConfiguration is injected into the 
+        // Startup constructor
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        // .net 3.1 for cors
+        // used for asp.net 3.1 cors issue
         readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        // used for Web APIs
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // used for Web APIs to add MVC Controllers, but not Razor pages and Views
+            // Adds services for Authorization, Validation, formattors, CORS, etc.
             services.AddControllers();
 
-            // .net 3.1 for cors
+            // Not needed for Web APIs as it adds both Views and Razor Pages,
+            // which are unnecessary for our Web API
+            // services.AddMvc();
+
+            services.AddDbContext<TodoContext>();
+            services.AddScoped<IRepository<Todo>, TodoRepository>();
+
+            // used for asp.net 3.1 cors issue
             services.AddCors(options =>
             {
                 options.AddPolicy(MyAllowSpecificOrigins,
@@ -41,35 +56,37 @@ namespace todos
                                         .AllowAnyMethod();
                 });
             });
-
-            // old .net method?
-            //services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
-            //{
-            //    builder.AllowAnyOrigin()
-            //            .AllowAnyMethod()
-            //            .AllowAnyHeader();
-            //}));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // used for any asp.net empty template
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            // .net 3.1 cors stuff?
+            // used for asp.net 3.1 cors issue
             app.UseCors(MyAllowSpecificOrigins);
 
+            // used for Web APIs, to ensure requests are made over secure domain
             app.UseHttpsRedirection();
 
+            // used for any asp.net empty template, it looks at incoming request
+            // and decides which endpoint should execute
             app.UseRouting();
 
+            // used for Web APIs
             app.UseAuthorization();
 
+            // used for any asp.net empty template to configure and execute endpoints, 
+            // endpoint routing is the preferred approach for Core versions 3.0 +
             app.UseEndpoints(endpoints =>
             {
+                // this Endpoint implementation used for Web APIs
+                // It only maps controllers that have routing attributes, 
+                // for example [Route("api/[controller]")] 
                 endpoints.MapControllers();
             });
         }
