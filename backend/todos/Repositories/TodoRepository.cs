25d1a46dbe3da57a4cb5a90f74cb2b92b97002ba
Jen E <jenevans3ten@gmail.com>
2020-03-17 20:27:39 -0400

    added TodoRepository

---
+++ backend/todos/Repositories/TodoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todos.Models;
+
+namespace todos.Repositories
+{
+    public class TodoRepository : Repository<Todo>, IRepository<Todo>
+    {
+        public TodoRepository(TodoContext context) : base(context)
+        {
+
+        }
+    }
+}
