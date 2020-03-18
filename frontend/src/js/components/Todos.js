39a09f51f702a7f2dced4df239a8d11affb8a06b
Jen E <jenevans3ten@gmail.com>
2020-03-18 14:19:52 -0400

    added edit button to todos component

--- frontend/src/js/components/Todos.js
+++ frontend/src/js/components/Todos.js
@@ -6,6 +6,7 @@ export default function Todos(todos){
             return `
             <li>
                 <h4 class="todo__items">${todo.name} by ${todo.owner}</h4>
+                <button class="edit-todo__edit">Edit</button>
                 <button class="delete-todo__submit">Delete</button>
                 <input class="todo__id" type="hidden" value="${todo.id}">
             </li>
