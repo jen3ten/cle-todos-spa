e98462d67a81ba15de8dc7313ad04c28089a53ae
Jen E <jenevans3ten@gmail.com>
2020-03-17 20:30:17 -0400

    added todo db context

---
+++ backend/todos/TodoContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todos.Models;
+
+namespace todos
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=TodoApiPractice;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+            //.UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasData(
+                new Todo
+                {
+                    Id = 1,
+                    Name = "Remodel Bathroom",
+                    Owner = "Mr. Handyman"
+                },
+                new Todo
+                {
+                    Id = 2,
+                    Name = "Clean out bedroom", 
+                    Owner = "Jen"
+                }
+            );
+        }
+    }
+}
