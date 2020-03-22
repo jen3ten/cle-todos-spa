6ef2e645d3d9b9b6cb36684738b4c6946685b76b
Jen E <jenevans3ten@gmail.com>
2020-03-22 18:49:28 -0400

    added owner repository

---
+++ backend/todos/Repositories/OwnerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todos.Models;
+
+namespace todos.Repositories
+{
+    public class OwnerRepository : Repository<Owner>, IRepository<Owner>
+    {
+        public OwnerRepository(TodoContext context) : base(context)
+        {
+
+        }
+    }
+}
