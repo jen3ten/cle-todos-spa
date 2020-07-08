950b110172e54cbb34885445b527128792b1758c
Jen E <jenevans3ten@gmail.com>
2020-07-08 15:05:12 -0400

    todo model, data context, seed data, migrations, sample controllers

---
+++ backend/todos/TodoContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todos.Models;
+
+namespace todos
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=TodoApiSummer2020;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+            //.UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasData(
+                new Todo
+                {
+                    Id = 1,
+                    Name = "Remodel Bathroom",
+                    Owner = "Mr. Handyman"
+                },
+                new Todo
+                {
+                    Id = 2,
+                    Name = "Clean out bedroom",
+                    Owner = "Jen"
+                },
+                new Todo
+                {
+                    Id = 3,
+                    Name = "Text Mom",
+                    Owner = "Sarah"
+                }
+            );
+        }
+    }
+}
