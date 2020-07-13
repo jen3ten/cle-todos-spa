46743a5b126e5753d98cc81e3e0a4c82bf35b17a
Jen E <jenevans3ten@gmail.com>
2020-07-13 19:41:44 -0400

    modified owners component to only display owner names

--- frontend/src/js/components/Owners.js
+++ frontend/src/js/components/Owners.js
@@ -4,17 +4,30 @@ export default function Owners(owners){
         <ul>
             ${owners.map(owner => {
                 return `
-                <li>${owner.name}'s Todos:
-                    <ol>
-                    ${owner.todos.map(todo =>{
-                        return`
-                        <li>${todo.name}</li>
-                        `
-                    }).join("")}
-                    </ol>
-                </li>
+                <li>${owner.name}</li>
                 `
             }).join("")}
         </ul>
     `
 }
+
+// export default function Owners(owners){
+//     return `
+//         <h1>List of Owners</h1>
+//         <ul>
+//             ${owners.map(owner => {
+//                 return `
+//                 <li>${owner.name}'s Todos:
+//                     <ol>
+//                     ${owner.todos.map(todo =>{
+//                         return`
+//                         <li>${todo.name}</li>
+//                         `
+//                     }).join("")}
+//                     </ol>
+//                 </li>
+//                 `
+//             }).join("")}
+//         </ul>
+//     `
+// }
