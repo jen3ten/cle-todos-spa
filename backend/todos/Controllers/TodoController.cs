ee2c2de06c74bfd087e3a6b4be00b021c3f10639
Jen E <jenevans3ten@gmail.com>
2020-11-11 10:38:41 -0500

    hooked up todo controller

---
+++ backend/todos/Controllers/TodoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using todos.Models;
+using todos.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace todos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private IRepository<Todo> todoRepo;
+
+        public TodoController(IRepository<Todo> todoRepo)
+        {
+            this.todoRepo = todoRepo;
+        }
+
+        // GET: api/<TodoController>
+        [HttpGet]
+        public IEnumerable<Todo> Get()
+        {
+            // return new string[] { "value1", "value2" };
+            return todoRepo.GetAll();
+        }
+
+        // GET api/<TodoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TodoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TodoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TodoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
