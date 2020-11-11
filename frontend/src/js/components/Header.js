4952a874b3d9568e94b1d6650e53abf2c5b2a3ed
Jen E <jenevans3ten@gmail.com>
2020-11-11 10:54:16 -0500

    components on front end

--- frontend/src/js/components/Header.js
+++ frontend/src/js/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return `<nav class='nav__header'>
         <ul>
-            <li>Home</li>
+            <li class=>Home</li>
             <li>ToDos</li>
             <li>Owners</li>
         </ul>
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,3 +1,7 @@
+// export default () => {
+//     document.querySelector('.app').innerText = "hello"
+// }
+
 // component imports
 import Header from './components/Header';
 
@@ -5,9 +9,21 @@ export default function pageBuild(){
     header();
 }
 
+//export default header();
+
 function header(){
     const headerElement = document.querySelector('.header');
     headerElement.innerHTML = Header();
+
+    // headerElement.innerHTML = 
+    // `<nav class="nav__header">
+    //     <ul>
+    //         <li>Home</li>
+    //         <li>ToDos</li>
+    //         <li>Owners</li>
+    //     </ul>
+    // </nav>
+    // `
 }
 
 
